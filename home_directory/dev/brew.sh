3cff903f062cf64fd4b86074735fc4b7858d45df
Shambhu Shrestha <shambhu.shrestha@beacon.io>
2017-10-19 16:56:14 +0900

    brew

---
+++ home_directory/dev/brew.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+
+Install following brew packages
+aspell
+autoconf
+coreutils
+exa
+fzf
+gdbm
+git
+icu4c
+libevent
+nginx
+node
+openssl
+openssl@1.1
+pcre
+pkg-config
+python3
+rbenv
+readline
+ruby-build
+sqlite
+tmux
+tree
+xz
+
